395a6b32a28a4d1e9ef5f7e641b1ea54c750ea2f
sasukeshyam <akashpandit790@gmail.com>
2026-09-19 12:33:02 +0530

    Added fix code in flight-controller

--- src/controller/flight-controller.js
+++ src/controller/flight-controller.js
@@ -6,13 +6,13 @@ const flightService = new FlightService();
 const createFlight = async (req, res) => {
     try {
         const flightRequestData = {
-            flightNumber = req.body.flightNumber,
-            airplaneId = req.body.airplaneId,
-            departureAirportId = req.body.departureAirportId,
-            arrivalAirportId = req.body.arrivalAirportId,
-            arrivalTime = req.body.arrivalTime,
-            departureTime = req.body.departureTime,
-            price = req.body.price
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
         }
         const flight = await flightService.createFlight(flightRequestData);
         return res.status(201).json({
